c79bcb63e5c1864755f4ece9d6e45e3875f7e676
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-24 23:02:48 +0700

    learn animated

---
+++ lib/AnimatedContainer.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyApp();
+}
+
+class _MyApp extends State<MyApp> {
+  Random random = Random();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Excercise Animated Container"),
+        ),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {});
+            },
+            child: AnimatedContainer(
+              color: Color.fromARGB(255, random.nextInt(256),
+                  random.nextInt(256), random.nextInt(256)),
+              duration: const Duration(seconds: 1),
+              width: 50.0 + random.nextInt(101),
+              height: 50.0 + random.nextInt(101),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
