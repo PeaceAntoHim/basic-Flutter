f6174fdb51cb9db80e54a899a3b95a7d8f1ad56e
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-24 22:22:52 +0700

    fix bug when create this app

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,26 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   // ignore: library_private_types_in_public_api
   _MyAppState createState() => _MyAppState();
 }
 
 // ignore: must_be_immutable
-class _MyAppState extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
   List<Widget> widgets = [];
 
 // This to made constructor of data
   _MyAppState() {
     for (int i = 0; i < 15; i++) {
-      widgets.add(Text("Data ke-$i"));
+      widgets.add(
+        Text(
+          "Data ke-$i",
+          style: const TextStyle(fontSize: 25),
+        ),
+      );
     }
   }
 
