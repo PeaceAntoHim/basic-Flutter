e419a5c3f10a57da7f31eac9317572ea3ba99796
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-26 22:13:21 +0700

    clean code

---
+++ lib/Basic_Flutter/Study_Case/Mutiplepage/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:basic_flutter/Mutiplepage/Login_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,3 @@
 import 'package:flutter/material.dart';
-import 'package:basic_flutter/Login_page.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage(),
-    );
-  }
-}
+void main() => runApp(MyApp());
