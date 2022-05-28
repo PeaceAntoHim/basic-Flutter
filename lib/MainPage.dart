5834c3f3fdd2fdb7c09f4416c3e959ccd1055df3
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-29 00:04:14 +0700

    learn to made card

---
+++ lib/MainPage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Custom Card Excercise",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFF8C062F),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFFFE5788), Color(0xFFF56D5D)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.7,
+              child: Card(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,15 @@
-// ignore_for_file: deprecated_member_use
-
+import 'package:basic_flutter/MainPage.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Excercise how to create inkwell"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              RaisedButton(
-                color: Colors.amber,
-                shape: const StadiumBorder(),
-                onPressed: () {},
-                child: const Text("Press Me"),
-              ),
-              Material(
-                borderRadius: BorderRadius.circular(20),
-                elevation: 2,
-                child: Container(
-                  width: 150,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    gradient: const LinearGradient(
-                      colors: [Colors.purple, Colors.pink],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                    ),
-                  ),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.transparent,
-                    child: InkWell(
-                      splashColor: Colors.amber,
-                      borderRadius: BorderRadius.circular(20),
-                      onTap: () {},
-                      child: const Center(
-                        child: Text(
-                          "My Button",
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: MainPage(),
     );
   }
 }
