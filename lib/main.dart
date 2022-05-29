b2859e6ddaa7df383c4b9786a8247f38000108e4
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-29 23:22:01 +0700

    create study case to create login

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,4 @@
-import 'package:basic_flutter/MainPage.dart';
+import 'package:basic_flutter/Basic_Flutter/AnimatedContainer.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
+void main() => runApp(MyApp());
