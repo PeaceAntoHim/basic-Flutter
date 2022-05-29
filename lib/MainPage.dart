bfa8b6ac8c596b8c77de899f4ff66174e383be88
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-29 23:32:58 +0700

    step one create background of the app

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -5,6 +5,28 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double getSmallDiameter(BuildContext context) =>
+        MediaQuery.of(context).size.width * 2 / 3;
+    double getBigDiameter(BuildContext context) =>
+        MediaQuery.of(context).size.height * 7 / 8;
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: getSmallDiameter(context),
+            height: getSmallDiameter(context),
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
