0793165b8b2927fd14c62ef9678b0126eb13aa63
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-29 23:25:23 +0700

    set environment

--- lib/Basic_Flutter/Study_Case/Cards/main.dart
+++ lib/Basic_Flutter/Study_Case/Cards/main.dart
@@ -1,4 +1,4 @@
-import 'package:basic_flutter/MainPage.dart';
+import 'package:basic_flutter/Cards/MainPage.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
-import 'package:basic_flutter/Basic_Flutter/AnimatedContainer.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'MainPage.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
