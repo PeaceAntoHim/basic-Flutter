f132de83fa733c407f2ad67905f66de52b93e98a
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-30 23:35:14 +0700

    fix edded topBar

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -8,20 +8,40 @@ class MainPage extends StatelessWidget {
     double getSmallDiameter(BuildContext context) =>
         MediaQuery.of(context).size.width * 2 / 3;
     double getBigDiameter(BuildContext context) =>
-        MediaQuery.of(context).size.height * 7 / 8;
+        MediaQuery.of(context).size.width * 7 / 8;
 
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          Container(
-            width: getSmallDiameter(context),
-            height: getSmallDiameter(context),
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
+          Positioned(
+            right: -getSmallDiameter(context) / 3,
+            top: -getSmallDiameter(context) / 3,
+            child: Container(
+              width: getSmallDiameter(context),
+              height: getSmallDiameter(context),
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  colors: [Color(0xFFB226B2), Color(0xFFFF6DA7)],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: -getSmallDiameter(context) / 4,
+            top: -getSmallDiameter(context) / 4,
+            child: Container(
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  colors: [Color(0xFFB226B2), Color(0xFFFF4891)],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
               ),
             ),
           ),
