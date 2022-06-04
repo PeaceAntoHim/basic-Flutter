f137dbdbed9212e1f043661be9f7c2f95e63357c
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-06-05 00:19:31 +0700

    was update an create email and pass

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -11,6 +11,7 @@ class MainPage extends StatelessWidget {
         MediaQuery.of(context).size.width * 7 / 8;
 
     return Scaffold(
+      backgroundColor: const Color(0xFFEEEEEE),
       body: Stack(
         children: <Widget>[
           Positioned(
@@ -33,6 +34,16 @@ class MainPage extends StatelessWidget {
             left: -getSmallDiameter(context) / 4,
             top: -getSmallDiameter(context) / 4,
             child: Container(
+              // ignore: sort_child_properties_last
+              child: const Center(
+                  child: Text(
+                "Frans",
+                style: TextStyle(
+                  fontFamily: "Macondo-Regular",
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              )),
               width: getBigDiameter(context),
               height: getBigDiameter(context),
               decoration: const BoxDecoration(
@@ -43,6 +54,86 @@ class MainPage extends StatelessWidget {
                   end: Alignment.bottomCenter,
                 ),
               ),
+            ),
+          ),
+          Positioned(
+            right: -getBigDiameter(context) / 2,
+            bottom: -getBigDiameter(context) / 2,
+            child: Container(
+              width: getBigDiameter(context),
+              height: getBigDiameter(context),
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Color(0xFFF3E9EE),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  margin: const EdgeInsets.fromLTRB(20, 300, 20, 10),
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 25),
+                  child: Column(
+                    children: const <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.email,
+                            color: Color(0xFFFF4891),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFFFF4891),
+                            ),
+                          ),
+                          labelText: "Email",
+                          labelStyle: TextStyle(
+                            color: Color(0xFFFF4891),
+                          ),
+                        ),
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          icon: Icon(
+                            Icons.vpn_key,
+                            color: Color(0xFFFF4891),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFFFF4891),
+                            ),
+                          ),
+                          labelText: "Password",
+                          labelStyle: TextStyle(
+                            color: Color(0xFFFF4891),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 20, 20),
+                    child: const Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        color: Color(0xFFFF4891),
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
